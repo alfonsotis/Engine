b20ed7e404cf35d7f13a30913c1eea3f274811ca
Alfonso Tiscornia <tiscorniaalfonso@gmail.com>
2023-10-01 18:12:44 +0200

    add animation to rotation and reset angle input

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var rotating_angle = +0;
 
+function resetAngle(){
+    rotating_angle = +0;
+    rotateEngine();
 
-function rotate(){
+}
+
+
+
+function rotateEngine(){
     engine = document.getElementById("engine");
     engine_angle = document.getElementById("angle").value
     rotating_angle = +rotating_angle + +engine_angle;
     console.log("rotating", rotating_angle);
-    engine.style.transform = `rotate(${rotating_angle}deg)`
+    engine.style.transform = `rotate(${rotating_angle}deg)`;
 }
