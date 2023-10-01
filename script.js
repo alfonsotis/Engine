b2482b58ec97576c50cd5f940e8ec8151ae3ae9d
Alfonso Tiscornia <tiscorniaalfonso@gmail.com>
2023-10-01 18:35:35 +0200

    Refactor setRotatingAngle.

--- script.js
+++ script.js
@@ -2,16 +2,30 @@ var rotating_angle = +0;
 
 function resetAngle(){
     rotating_angle = +0;
-    rotateEngine();
+    document.getElementById("angle").value = 0;
+    applyRotation();
+}
 
+function getAngle(){
+    return document.getElementById("angle").value
+}
+
+function getEngine(){
+    return document.getElementById("engine");
+}
+
+function applyRotation(){
+    motor = getEngine();
+    motor.style.transform = `rotate(${rotating_angle}deg)`;
+}
+
+function setRotatingAngle(engine_angle){
+    rotating_angle = +rotating_angle + +engine_angle;
 }
 
 
-
 function rotateEngine(){
-    engine = document.getElementById("engine");
-    engine_angle = document.getElementById("angle").value
-    rotating_angle = +rotating_angle + +engine_angle;
-    console.log("rotating", rotating_angle);
-    engine.style.transform = `rotate(${rotating_angle}deg)`;
+    engine_angle = getAngle();
+    setRotatingAngle(engine_angle);
+    applyRotation();
 }
