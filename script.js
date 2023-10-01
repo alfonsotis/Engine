8171c672974b66aad6da733f43090d044f3e8940
Alfonso Tiscornia <tiscorniaalfonso@gmail.com>
2023-10-01 17:45:06 +0200

    Allow multiple rotations

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+var rotating_angle = +0;
+
 
 function rotate(){
-    console.log("rotating");
     engine = document.getElementById("engine");
-    engine.style.transform = 'rotate(90deg)'
+    engine_angle = document.getElementById("angle").value
+    rotating_angle = +rotating_angle + +engine_angle;
+    console.log("rotating", rotating_angle);
+    engine.style.transform = `rotate(${rotating_angle}deg)`
 }
